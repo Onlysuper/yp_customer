fe85c5b509689f5e96105bac66c934fa26120c68
zhiyong <zhiyong.chen@lefu8.com>
2017-12-14 17:59:33 +0800

    修改路径

--- src/router/home/index.js
+++ src/router/home/index.js
@@ -9,7 +9,7 @@ export default {
   path: "/home",
   component: Layout,
   children: [{
-    path: '/home',
+    path: 'home',
     component: r => {
       Loading(true);
       return require.ensure([], () => { Loading(false); return r(require("@src/views/Home/Home" + isMobile)) }, "home")
