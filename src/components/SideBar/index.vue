ead0f952cdde0983660b7582370dfdb870ad41c7
xuyu <2483613372@qq.com>
2018-01-18 13:48:13 +0800

    样式

--- src/components/SideBar/index.vue
+++ src/components/SideBar/index.vue
@@ -27,7 +27,7 @@
   <!-- 左侧菜单 -->
 </template>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang='less' scoped>
+<style lang='less'>
 .my-transition(@attr) {
   transition: @attr 0.8s;
   -moz-transition: @attr 0.8s;
@@ -115,40 +115,42 @@
 }
 
 @media screen and (min-width: 500px) {
-  // .aside-box {
-  .logo-box {
-    height: 54px;
-    // min-width: 70px;
-    overflow: hidden;
-    display: flex;
-    box-sizing: border-box;
-    justify-content: start;
-    align-items: center;
-    padding: 0 16px;
-    background: #002240;
-    color: #fff;
-    box-shadow: 0px 0px 8px rgba(105, 105, 105, 0.2);
-    .img-box {
-      display: inline-block;
-      img {
-        height: 30px;
+  .aside-box {
+    // .aside-box {
+    .logo-box {
+      height: 54px;
+      // min-width: 70px;
+      overflow: hidden;
+      display: flex;
+      box-sizing: border-box;
+      justify-content: start;
+      align-items: center;
+      padding: 0 16px;
+      background: #002240;
+      color: #fff;
+      box-shadow: 0px 0px 8px rgba(105, 105, 105, 0.2);
+      .img-box {
+        display: inline-block;
+        img {
+          height: 30px;
+        }
       }
-    }
-    .home-title {
-      padding: 0px;
-      line-height: 0;
-      display: inline-block;
-      padding-left: 20px;
-      padding-left: 10px;
-      font-size: 18px;
-      font-weight: 400;
-      white-space: nowrap;
-      -webkit-touch-callout: none; /* iOS Safari */
-      -webkit-user-select: none; /* Chrome/Safari/Opera */
-      -khtml-user-select: none; /* Konqueror */
-      -moz-user-select: none; /* Firefox */
-      -ms-user-select: none; /* Internet Explorer/Edge */
-      user-select: none;
+      .home-title {
+        padding: 0px;
+        line-height: 0;
+        display: inline-block;
+        padding-left: 20px;
+        padding-left: 10px;
+        font-size: 18px;
+        font-weight: 400;
+        white-space: nowrap;
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Chrome/Safari/Opera */
+        -khtml-user-select: none; /* Konqueror */
+        -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none;
+      }
     }
   }
 }
