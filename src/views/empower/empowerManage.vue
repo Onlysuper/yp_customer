4e6d938e7ada084b1c2bcedc428299a284d6e890
xuyu <2483613372@qq.com>
2018-02-09 15:21:35 +0800

    授权码管理高级搜索出现滚动条

--- src/views/empower/empowerManage.vue
+++ src/views/empower/empowerManage.vue
@@ -3,7 +3,7 @@
   <div class="admin-page empower">
     <div class="admin-main-box">
       <!-- search form start -->
-      <myp-search-form @changeform="callbackformHandle" @resetInput="resetSearchHandle" @resetSome="resetSomeInputHandle" @visiblesome="visiblesomeHandle" @seachstart="seachstartHandle" :searchOptions="searchOptions"></myp-search-form>
+      <myp-search-form @changeform="callbackformHandle" @resetInput="resetSearchHandle" @resetSome="resetSomeInputHandle" @visiblesome="visiblesomeHandle" @seachstart="seachstartHandle" :searchOptions="searchOptions" @changeSearchVisible="changeSearchVisible"></myp-search-form>
       <!-- search form end -->
       <div class="operation-box">
         <el-button-group class="button-group">
