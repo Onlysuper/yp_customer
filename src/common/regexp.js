17647e1c4d318f739128907c162126ecd45e3c56
xuyu <2483613372@qq.com>
2018-03-14 16:34:53 +0800

    注册资金必须为正数

--- src/common/regexp.js
+++ src/common/regexp.js
@@ -6,7 +6,8 @@ const regRule = {
     money: /^([1-9][\d]{0,7}|0)(\.[\d]{1,2})?$/,
     name: /^([\u4e00-\u9fa5]+|[a-zA-Z0-9]+)$/,
     bankno: /^\d{16,19}$/,
-    idCard: /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/ // 身份证号码
+    idCard: /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/, // 身份证号码
+    positiveNum: /^([0-9]*[0-9][0-9]*(.[0-9]+)?|[0]+.[0-9]*[0-9][0-9]*)$/ // 身份证号码
 }
 // 税号 可以为空
 const taxNumVerify = (rule, value, callback) => {
@@ -56,11 +57,21 @@ const taxNumVerify_r = (rule, value, callback) => {
         callback();
     }
 };
+// 注册资金 不可为空 并且必须为证书
+const registMoney_r = (rule, value, callback) => {
+    let val = value == 0 ? "0" : value;
+    if (!val || !regRule['positiveNum'].test(value)) {
+        return callback(new Error("注册资金需大于0"));
+    } else {
+        callback();
+    }
+};
 export {
     taxNumVerify,
     idCardVerify,
     phoneNumVerify,
     phoneNumVerify_r,
     idCardVerify_r,
-    taxNumVerify_r
+    taxNumVerify_r,
+    registMoney_r
 }
