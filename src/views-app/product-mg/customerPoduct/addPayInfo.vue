f510739bda6c888053e5724f591ec0e75c491704
zewei <543347844@qq.com>
2018-04-13 15:54:58 +0800

    支付开通表单增加自动缓存功能

--- src/views-app/product-mg/customerPoduct/addPayInfo.vue
+++ src/views-app/product-mg/customerPoduct/addPayInfo.vue
@@ -14,9 +14,9 @@
             <mt-field label="所在地区:" type="text" v-model="city.resultAddr" @click.native="cityVisible = true" placeholder="选择地区" v-readonly-ios :readonly="true">
               <i class="icon-arrow"></i>
             </mt-field>
-            <mt-field label="详细地址:" type="text" v-model="form.bussinessAddress" placeholder="例如：古美路18号" v-required :attr="{maxlength:50}"></mt-field>
-            <mt-field label="法人:" type="text" v-model="form.legalPerson" placeholder="输入法人姓名" v-required :attr="{maxlength:50}"></mt-field>
-            <mt-field label="身份证号:" type="text" v-model="form.idCard" placeholder="输入身份证号" v-required :attr="{maxlength:50}"></mt-field>
+            <mt-field label="详细地址:" type="text" v-model="form.bussinessAddress" @change="cacheFrom" placeholder="例如：古美路18号" v-required :attr="{maxlength:50}"></mt-field>
+            <mt-field label="法人:" type="text" v-model="form.legalPerson" @change="cacheFrom" placeholder="输入法人姓名" v-required :attr="{maxlength:50}"></mt-field>
+            <mt-field label="身份证号:" type="text" v-model="form.idCard" @change="cacheFrom" placeholder="输入身份证号" v-required :attr="{maxlength:50}"></mt-field>
             <mt-field label="行业类别:" type="text" v-model="bussinessType.name" @click.native="$refs.bussinessType.open" placeholder="选择行业类别" v-readonly-ios :readonly="true">
               <i class="icon-arrow"></i>
             </mt-field>
@@ -29,15 +29,15 @@
             <!-- <mt-field label="邮箱:" type="email" v-model="form.contactEmail" placeholder="接收开通信息（选填）" :attr="{maxlength:50}"></mt-field> -->
             <mt-radio title="结算信息" v-model="form.accountType" :options="[{ label: '对公',value: '0' },{ label: '对私',value: '1' }]" class="mint-radiolist-row border-1px"></mt-radio>
             <!-- 对公显示,带入企业名称,不可更改。对私显示,带入法人名称,可更改-->
-            <mt-field label="账户名称:" type="text" v-model="form.accountName" placeholder="请输入账户名称" :disabled="form.accountType == '0'"></mt-field>
+            <mt-field label="账户名称:" type="text" v-model="form.accountName" @change="cacheFrom" placeholder="请输入账户名称" :disabled="form.accountType == '0'"></mt-field>
             <mt-field label="开户银行:" type="text" v-model="bank.value" @click.native="bankVisible = true" placeholder="选择开户银行" v-readonly-ios :readonly="true">
               <i class="icon-arrow"></i>
             </mt-field>
             <mt-field label="开户支行:" type="text" v-model="bankBranch.branchName" @click.native="openBankBranch" placeholder="选择开户支行" v-readonly-ios :readonly="true">
               <i class="icon-arrow"></i>
             </mt-field>
-            <mt-field label="帐号:" type="tel" v-model="form.accountNo" placeholder="请输入帐号" :attr="{maxlength:50}"></mt-field>
-            <mt-field label="预留手机号:" type="tel" v-model="form.reservedPhoneNo" placeholder="请输入银行预留手机号" :attr="{maxlength:11}"></mt-field>
+            <mt-field label="帐号:" type="tel" v-model="form.accountNo" @change="cacheFrom" placeholder="请输入帐号" :attr="{maxlength:50}"></mt-field>
+            <mt-field label="预留手机号:" type="tel" v-model="form.reservedPhoneNo" @change="cacheFrom" placeholder="请输入银行预留手机号" :attr="{maxlength:11}"></mt-field>
           </input-wrapper>
         </view-radius>
       </div>
@@ -87,6 +87,7 @@ export default {
       enterpriseName: "",
       taxNo: "",
       form: {
+        bussinessAddress: "",
         accountType: "",
         legalPerson: "",
         bussinessLicenseEffectiveBegin: "",
@@ -110,6 +111,8 @@ export default {
     "form.accountType"(v) {
       if (v == "0") this.form.accountName = this.enterpriseName;
       else this.form.accountName = this._accountName == this.enterpriseName ? this.form.legalPerson : this._accountName || this.form.legalPerson;
+
+      this.cacheFrom(); //缓存数据
     }
   },
   created() {
@@ -132,36 +135,46 @@ export default {
   },
   methods: {
     echoForm(data) {
+      let cacheForm = {};
       let { customer, settleCard } = data;
       if (customer instanceof Object) {
-        this.enterpriseName = customer.enterpriseName;
-        this.taxNo = customer.taxNo;
-        let city = this.$refs.CityPicher.findCity(customer.orgCode);
+        /*
+        * 回显表单逻辑必须是优先从缓存中取
+        */
+        cacheForm = JSON.parse(window.localStorage.getItem(customer.customerNo)) || {};//取出缓存表单信息
+        // console.log("取出", cacheForm)
+
+        this.enterpriseName = cacheForm.enterpriseName || customer.enterpriseName;
+        this.taxNo = cacheForm.taxNo || customer.taxNo;
+
+        let city = this.$refs.CityPicher.findCity(cacheForm.orgCode || customer.orgCode);
         this.resultCallback(city);
-        this.form.legalPerson = customer.legalPerson;
-        this.form.bussinessAddress = customer.bussinessAddress;
-        this.form.idCard = customer.idCard;
-        // this.form.bussinessLicenseEffectiveBegin = customer.bussinessLicenseEffectiveBegin;
-        customer.bussinessLicenseEffectiveBegin && this.setStartDate(new Date(customer.bussinessLicenseEffectiveBegin));
-        // this.form.bussinessLicenseEffectiveEnd = customer.bussinessLicenseEffectiveEnd;
-        customer.bussinessLicenseEffectiveEnd && this.setEndDate(new Date(customer.bussinessLicenseEffectiveEnd));
-        this.form.contactEmail = customer.contactEmail;
-        let bussinessType = bussinessTypeJson.find(item => item.code == customer.category);
+        this.form.legalPerson = cacheForm.legalPerson || customer.legalPerson;
+        this.form.bussinessAddress = cacheForm.bussinessAddress || customer.bussinessAddress;
+        this.form.idCard = cacheForm.idCard || customer.idCard;
+        let bussinessLicenseEffectiveBegin = cacheForm.bussinessLicenseEffectiveBegin || customer.bussinessLicenseEffectiveBegin;
+        bussinessLicenseEffectiveBegin && this.setStartDate(new Date(bussinessLicenseEffectiveBegin));
+        let bussinessLicenseEffectiveEnd = cacheForm.bussinessLicenseEffectiveEnd || customer.bussinessLicenseEffectiveEnd;
+        bussinessLicenseEffectiveEnd && this.setEndDate(new Date(bussinessLicenseEffectiveEnd));
+        this.form.contactEmail = cacheForm.contactEmail || customer.contactEmail;
+        let category = cacheForm.category || customer.category;
+        let bussinessType = bussinessTypeJson.find(item => item.code == category);
         this.confirm(bussinessType || {});
       }
+      settleCard = settleCard || {};
       if (settleCard instanceof Object) {
         this.bankResult({
-          value: settleCard.bankName,
-          key: settleCard.bankCode
+          value: cacheForm.bankName || settleCard.bankName,
+          key: cacheForm.bankCode || settleCard.bankCode
         });
         this.bankRsearchResult({
-          branchName: settleCard.branchName,
-          unionCode: settleCard.unionCode
+          branchName: cacheForm.branchName || settleCard.branchName,
+          unionCode: cacheForm.unionCode || settleCard.unionCode
         });
-        this.form.accountType = settleCard.accountType;
-        this.form.accountName = this._accountName = settleCard.accountName;
-        this.form.accountNo = settleCard.accountNo;
-        this.form.reservedPhoneNo = settleCard.reservedPhoneNo;
+        this.form.accountType = cacheForm.accountType || settleCard.accountType;
+        this.form.accountName = this._accountName = cacheForm.accountName || settleCard.accountName;
+        this.form.accountNo = cacheForm.accountNo || settleCard.accountNo;
+        this.form.reservedPhoneNo = cacheForm.reservedPhoneNo || settleCard.reservedPhoneNo;
       } else {
         // this.form.accountType = "0"; //如果需要默认值
       }
@@ -174,11 +187,13 @@ export default {
     //地区选择回调函数
     resultCallback(obj) {
       this.city = obj;
+      this.cacheFrom(); //缓存数据
     },
 
     //行业类别
     confirm(obj) {
       this.bussinessType = obj;
+      this.cacheFrom(); //缓存数据
     },
 
     //选择银行
@@ -186,6 +201,7 @@ export default {
       this.bank = bank;
       this.bankBranch = {};
       this.bankBranchQuery = {};
+      this.cacheFrom(); //缓存数据
     },
 
     //打开支行
@@ -212,18 +228,20 @@ export default {
       this.bankSearchVisible = false;
       this.bankBranchVisible = false;
       this.bankBranch = resultObj;
+      this.cacheFrom(); //缓存数据
     },
     setStartDate(date) {
       this.form.bussinessLicenseEffectiveBegin = utils.formatDate(date, "yyyy-MM-dd");
       this.bussinessLicenseEffectiveBeginVal = date;
+      this.cacheFrom(); //缓存数据
     },
     setEndDate(date) {
       this.form.bussinessLicenseEffectiveEnd = utils.formatDate(date, "yyyy-MM-dd");
       this.bussinessLicenseEffectiveEndVal = date;
+      this.cacheFrom(); //缓存数据
     },
-    //提交
-    submit() {
-      let form = {
+    sendParams() {
+      return {
         ...this.form,
         customerNo: this.customerNo,
         orgCode: this.city.resultCode,
@@ -233,10 +251,24 @@ export default {
         bankCode: this.bank.key,
         bankName: this.bank.value
       };
+    },
+    //save from to localStorage
+    cacheFrom() {
+      window.localStorage.setItem(this.customerNo, JSON.stringify(this.sendParams()));
+      // console.log(JSON.stringify(form))
+    },
+    clearcacheFrom() {
+      window.localStorage.removeItem(this.customerNo);
+    },
+    //提交
+    submit() {
+
+      let form = this.sendParams();
       // console.log(form);
 
       completeSettleInfo()(form).then(data => {
         if (data.code == "00") {
+          this.clearcacheFrom();
           this.$router.push({
             path: "./addGoods",
             query: { customerNo: this.customerNo }
